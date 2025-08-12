a4a62995ec0bd20db43257154b456c7f3dfa11c0
yantowsky <it.yantowsky@gmail.com>
2025-08-12 09:48:00 +0300

    Add Home Work 5.2

---
+++ Home_Works/pb_hw_5_2.py
@@ -0,0 +1,31 @@
+working_calc = 'y'
+while working_calc == 'y' or working_calc == 'yes':
+
+    first_number = int(input('Enter first number: '))
+    second_number = int(input('Enter second number: '))
+    calculator_action = input('Enter calculator action (+ - * /): ')
+
+    result = 0
+    message = ''
+
+    if calculator_action == '+':
+        result = first_number + second_number
+    elif calculator_action == '-':
+        result = first_number - second_number
+    elif calculator_action == '*':
+        result = first_number * second_number
+    elif calculator_action == '^':
+        result = first_number ** second_number
+    elif calculator_action == '/' and second_number != 0:
+        result = first_number / second_number
+    elif calculator_action == '/' and second_number == 0:
+        message = f'Number {first_number} cannot be divided by zero'
+    else:
+        message = 'You entered invalid calculator action'
+
+    if message:
+        print(message)
+    else:
+        print(f'{first_number} {calculator_action} {second_number} = {result}')
+
+    working_calc = input('You need a calculator? (yes/no): ')
