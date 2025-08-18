d012c5563c6dbd3b49d2714d68ecb4b7caac2201
yantowsky <it.yantowsky@gmail.com>
2025-08-18 21:37:57 +0300

    Add Home Work 7.2

---
+++ Home_Works/pb_hw_7_2.py
@@ -0,0 +1,12 @@
+def correct_sentence(text: str) -> str:
+    mod_string = text[0].upper() + text[1:] + ('.' if text[-1] != '.' else '')
+
+    return mod_string
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
