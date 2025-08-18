d21d6f2ad460357a15ed1759a39c01d7f04fdb34
yantowsky <it.yantowsky@gmail.com>
2025-08-18 15:20:10 +0300

    Corrected variable names

--- Home_Works/pb_hw_6_1.py
+++ Home_Works/pb_hw_6_1.py
@@ -4,14 +4,14 @@ user_letters = input('Please enter two letters separated by a hyphen (Example: a
 
 start_letter, end_letter = user_letters.split('-')
 
-index_start_later = string.ascii_letters.index(start_letter)
-index_end_later = string.ascii_letters.index(end_letter)
+index_start_letter = string.ascii_letters.index(start_letter)
+index_end_letter = string.ascii_letters.index(end_letter)
 
 mod_string_letters = ''
 
-if index_start_later > index_end_later:
-    mod_string_letters = string.ascii_letters[index_start_later:] + string.ascii_letters[:index_end_later + 1]
+if index_start_letter > index_end_letter:
+    mod_string_letters = string.ascii_letters[index_start_letter:] + string.ascii_letters[:index_end_letter + 1]
 else:
-    mod_string_letters = string.ascii_letters[index_start_later:index_end_later + 1]
+    mod_string_letters = string.ascii_letters[index_start_letter:index_end_letter + 1]
 
 print(f'"{user_letters}" -> {mod_string_letters}')
