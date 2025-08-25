c25915018147f3b2f6dd2cb63c4c59d4121ba329
yantowsky <it.yantowsky@gmail.com>
2025-08-25 23:23:27 +0300

    Add Home Work 9.1

---
+++ Home_Works/pb_hw_9_1.py
@@ -0,0 +1,11 @@
+def popular_words(text, words):
+    list_text = text.lower().split()
+
+    dict_word_count = {word: list_text.count(word) for word in words}
+
+    return dict_word_count
+
+
+assert (popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                      ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}), 'Test1'
+print('OK')
