d2e31411b1daeb1319ff1f23e13eabc4758bfeaf
yantowsky <it.yantowsky@gmail.com>
2025-09-03 16:44:11 +0300

    Added a second solution

--- Home_Works/pb_hw_11_3.py
+++ Home_Works/pb_hw_11_3.py
@@ -1,9 +1,13 @@
-def is_even(number):
-    list_last_digit_even = [0, 2, 4, 6, 8]
+def is_even(number: int) -> bool:
+    ##################  Option #1  ###################
+    # list_last_digit_even = [0, 2, 4, 6, 8]
+    #
+    # last_digit_in_num = int(str(number)[-1])
+    #
+    # return last_digit_in_num in list_last_digit_even
 
-    last_digit_in_num = int(str(number)[-1])
-
-    return last_digit_in_num in list_last_digit_even
+    #################  Option #2  ####################
+    return not number & 1
 
 
 assert is_even(2494563894038 ** 2) == True, 'Test1'
